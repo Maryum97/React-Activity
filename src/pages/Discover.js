327e37cff8309dbf746a362b5748b469850a0e81
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-06 16:03:43 +0100

    pushed app to github

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import API from "../../utils/API";
+import API from "../utils/API";
 import Card from "../components/Card";
 import Alert from "../components/Alert";
 
